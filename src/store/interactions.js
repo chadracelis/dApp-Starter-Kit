aec3a2f088ca7a627d0c954d43f6410957cb35c2
Chad <49378047+chadracelis@users.noreply.github.com>
2019-08-17 20:52:23 -0400

    Invoked contract events

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -3,12 +3,12 @@
 ///////////////////////// and to our actions to update the state. ////////////////////////////////////////
 
 ///////////////// Any interactions such as triggering methods will be passed here  ///////////////////////
-////////////////// from our components where we call upon web3 to handle for us //////////////////////////
+//////////////// from our components where we can call upon web3 to handle for us ////////////////////////
 
 /////////////// <- Our data will pass through our store in this order -> /////////////////////////////////
-/////////////// 1. interactions - handle interactions and intiailize web3 and contract ///////////////////
+/////////////// 1. interactions - hanldle interactions and intiailize web3 and contract //////////////////
 /////////////// 2. actions - handle our interactions and pass to reducers ////////////////////////////////
-/////////////// 3. reducers - retrieve actions and update the state in the store /////////////////////////
+/////////////// 3. reducers - retrieve actions and update the state //////////////////////////////////////
 /////////////// 4. selectors - fetch the state from our store ////////////////////////////////////////////
 
 import Web3 from 'web3';
@@ -16,6 +16,7 @@ import {
   web3Loaded,
   web3AccountLoaded,
   contractLoaded,
+  contractEventsLoaded
 } from './actions';
 import SampleContract from '../abis/SampleContract.json';
 
@@ -45,4 +46,16 @@ export const loadContract = async (web3, networkId, dispatch) => {
     window.alert('Contract not deployed to the current network. Please select another network with Metamask.')
     return null
   }
+}
+
+// LOAD CONTRACT EVENTS
+export const loadContractEvents = async (contract, dispatch) => {
+  // Fetch events with 'dogAdded' event 
+  const addedDogs = await contract.getPastEvents('dogAdded', { fromBlock: 0,  toBlock: 'latest' })
+  console.log(addedDogs)
+  // Format events
+  const dogs = addedDogs.map((event) => event.returnValues)
+  // Add events to redux store
+  dispatch(contractEventsLoaded(dogs))
+  return dogs
 }
